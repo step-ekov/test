86ef7285245546f93f91b9be0a14c984401e5bd1
step-ekov <150891349+step-ekov@users.noreply.github.com>
2023-11-15 10:27:15 +0300

    Add files via upload

---
+++ Zher_git/test_git1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_git1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Паша еврей!!!");
+            Pasha();
+        }
+
+        public static void Pasha()
+        {
+            int[] gruppa = new int[24];
+
+            for(int i = 0; i > gruppa.Length; i++)
+            {
+                gruppa[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int j = 0; j > gruppa.Length; j++)
+            {
+                Console.WriteLine(gruppa[j]);
+            }
+        }
+    }
+}
