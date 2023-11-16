fe237731848ee71db3278d985990dc3a1571185a
step-ekov <150891349+step-ekov@users.noreply.github.com>
2023-11-16 10:11:04 +0300

    Add files via upload

---
+++ MathQuiz 1/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace MathQuiz_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.plusLeftLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sum = new System.Windows.Forms.NumericUpDown();
+            this.minusLeftLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.minusRightLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.difference = new System.Windows.Forms.NumericUpDown();
+            this.timesLeftLabel = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timesRightLabel = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.product = new System.Windows.Forms.NumericUpDown();
+            this.dividedLeftLabel = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dividedRightLabel = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.quotient = new System.Windows.Forms.NumericUpDown();
+            this.startButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.difference)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.product)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quotient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.TimeLabel, "TimeLabel");
+            this.TimeLabel.Name = "TimeLabel";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // plusLeftLabel
+            // 
+            resources.ApplyResources(this.plusLeftLabel, "plusLeftLabel");
+            this.plusLeftLabel.Name = "plusLeftLabel";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // sum
+            // 
+            resources.ApplyResources(this.sum, "sum");
+            this.sum.Name = "sum";
+            // 
+            // minusLeftLabel
+            // 
+            resources.ApplyResources(this.minusLeftLabel, "minusLeftLabel");
+            this.minusLeftLabel.Name = "minusLeftLabel";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // minusRightLabel
+            // 
+            resources.ApplyResources(this.minusRightLabel, "minusRightLabel");
+            this.minusRightLabel.Name = "minusRightLabel";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // difference
+            // 
+            resources.ApplyResources(this.difference, "difference");
+            this.difference.Name = "difference";
+            // 
+            // timesLeftLabel
+            // 
+            resources.ApplyResources(this.timesLeftLabel, "timesLeftLabel");
+            this.timesLeftLabel.Name = "timesLeftLabel";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // timesRightLabel
+            // 
+            resources.ApplyResources(this.timesRightLabel, "timesRightLabel");
+            this.timesRightLabel.Name = "timesRightLabel";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // product
+            // 
+            resources.ApplyResources(this.product, "product");
+            this.product.Name = "product";
+            // 
+            // dividedLeftLabel
+            // 
+            resources.ApplyResources(this.dividedLeftLabel, "dividedLeftLabel");
+            this.dividedLeftLabel.Name = "dividedLeftLabel";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // dividedRightLabel
+            // 
+            resources.ApplyResources(this.dividedRightLabel, "dividedRightLabel");
+            this.dividedRightLabel.Name = "dividedRightLabel";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // quotient
+            // 
+            resources.ApplyResources(this.quotient, "quotient");
+            this.quotient.Name = "quotient";
+            // 
+            // startButton
+            // 
+            resources.ApplyResources(this.startButton, "startButton");
+            this.startButton.Name = "startButton";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.quotient);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.dividedRightLabel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.dividedLeftLabel);
+            this.Controls.Add(this.product);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.timesRightLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.timesLeftLabel);
+            this.Controls.Add(this.difference);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.minusRightLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.minusLeftLabel);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.plusLeftLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TimeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.difference)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.product)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quotient)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label plusLeftLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown sum;
+        private System.Windows.Forms.Label minusLeftLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label minusRightLabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown difference;
+        private System.Windows.Forms.Label timesLeftLabel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label timesRightLabel;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.NumericUpDown product;
+        private System.Windows.Forms.Label dividedLeftLabel;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label dividedRightLabel;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.NumericUpDown quotient;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
